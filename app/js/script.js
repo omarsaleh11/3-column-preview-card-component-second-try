5bac644ab9d30bd7fe46afe0877a5adab87164d6
omarsaleh11 <osaleh24724@gmail.com>
2022-09-17 18:33:47 +0200

    u

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,15 @@
-const btnEl = document.getElementById("btn-el");
-const sectionOne = document.getElementsByClassName("section-1");
-btnEl.addEventListener("click", () => {
+const btnElOne = document.getElementById("btn-el-1");
+const btnElTwo = document.getElementById("btn-el-2");
+const btnElThree = document.getElementById("btn-el-3");
+const sectionOne = document.getElementById("section-1");
+const sectionTwo = document.getElementById("section-2");
+const sectionThree = document.getElementById("section-3");
+btnElOne.addEventListener("click", () => {
  sectionOne.classList.toggle("js");
+});
+btnElTwo.addEventListener("click", () => {
+ sectionTwo.classList.toggle("js");
+});
+btnElThree.addEventListener("click", () => {
+ sectionThree.classList.toggle("js");
 });
