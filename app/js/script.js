e0ab7965e142378dc50381abeac2f19711f110bd
omarsaleh11 <osaleh24724@gmail.com>
2022-09-18 13:46:51 +0200

    update: remove events by clicking on bg

--- app/js/script.js
+++ app/js/script.js
@@ -6,6 +6,7 @@ const sectionTwo = document.getElementById("section-2");
 const sectionThree = document.getElementById("section-3");
 const logo = document.getElementById("logo");
 const about = document.getElementById("about");
+const removeEvents = document.getElementById("remove-events");
 const mainEl = document.getElementById("main");
 logo.addEventListener("click", () => {
  about.classList.toggle("none");
@@ -27,4 +28,14 @@ btnElThree.addEventListener("click", () => {
  sectionTwo.classList.toggle("none");
  sectionOne.classList.toggle("none");
  mainEl.classList.toggle("width");
+});
+removeEvents.addEventListener("click", () => {
+ sectionOne.classList.remove("js");
+ sectionOne.classList.remove("none");
+ sectionTwo.classList.remove("js");
+ sectionTwo.classList.remove("none");
+ sectionThree.classList.remove("js");
+ sectionThree.classList.remove("none");
+ mainEl.classList.remove("width");
+ about.classList.add("none");
 });
